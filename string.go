5b02cd42f464da3de5105f07dddce3d580cce54d
driftkill <vila.vishal@gmail.com>
2019-09-30 15:50:35 +0530

    Done

--- string.go
+++ string.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	s := "ABCDWXYZ abcdwxyz"
+	s := "ABCDWX abcdwx"
 	bs := []byte(s)
 	fmt.Println(s, bs)
 
 	for i := 0; i < len(s); i++ {
 		fmt.Printf("%#U ", s[i])
 	}
+	sum := 0
+	s1 := "vishal"
+	for k := 0; k < len(s1); k++ {
+		sum = sum + int(s1[k])
+	}
+	fmt.Println("")
+	fmt.Println(sum)
 }
