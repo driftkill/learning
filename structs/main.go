dc8ce64bf7cb81e7f51a967826ec9e0412033fb9
driftkill <vila.vishal@gmail.com>
2019-10-03 16:46:09 +0530

    Done

---
+++ structs/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	fav   []string
+}
+
+func main() {
+	p1 := person{
+		first: "Vishal",
+		last:  "Kumar",
+		fav: []string{"Chocolate", "Vanila"},
+	}
+	p2 := person{
+		first : "Jay",
+		last : "Sharma",
+		fav : []string{"Curent", "Butter"}
+	}
+	fmt.Println(p1)
+	fmt.Println(p2)
+}
