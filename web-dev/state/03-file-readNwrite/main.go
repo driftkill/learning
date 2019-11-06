ad512ed52b52e35327030ee624bbe5a7c10c60e5
driftkill <vila.vishal@gmail.com>
2019-11-06 19:05:44 +0530

    Done

---
+++ web-dev/state/03-file-readNwrite/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("template/*"))
+}
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	var s string
+	fmt.Println(r.Method)
+	if r.Method == http.MethodPost {
+		f, h, err := r.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		fmt.Println("/nFile: ", f, "/nHeader: ", h, "/nError: ", err)
+
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		s = string(bs)
+
+		fmt.Println(s)
+
+		bst, err := os.Create(filepath.Join("./user/", h.Filename))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer bst.Close()
+
+		_, err = bst.Write(bs)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl.ExecuteTemplate(w, "index.gohtml", s)
+}
